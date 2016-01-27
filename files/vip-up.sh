5766e248d7903090399050f38c4d5f66d16f1ed8
Sebastien Perreault <sperreault@alesium.net>
2016-01-27 17:26:02 -0500

    better vip-up and down script

--- files/vip-up.sh
+++ files/vip-up.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+dir=$(dirname $0)
+. $dir/vip-functions
 
-ifconfig "$1":"$3" up
+INTERFACE="$1":"$3"
+IP=$2
+
+sleep 3
+if [ $(get_interface_status $INTERFACE) == 255 ]; then
+        plumb_interface $INTERFACE
+        assign_interface_ip $INTERFACE $IP
+fi
+up_interface $INTERFACE 
+    
+    
